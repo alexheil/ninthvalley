1dfe03689acdf50c46eb11374bb82bb6a5b50ccc
alexheil <aheil4250@gmail.com>
2018-05-18 17:07:00 -0400

    categories

--- db/migrate/20180518205401_create_categories.rb
+++ db/migrate/20180518205401_create_categories.rb
@@ -1,6 +1,10 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+      t.string :slug
 
       t.timestamps
     end
--- db/migrate/20180518205409_create_subcategories.rb
+++ db/migrate/20180518205409_create_subcategories.rb
@@ -1,6 +1,10 @@
 class CreateSubcategories < ActiveRecord::Migration[5.0]
   def change
     create_table :subcategories do |t|
+      t.string :title
+      t.text :description
+      t.string :image
+      t.string :slug
 
       t.timestamps
     end
