b6c8b81bded75bc3fe217792335daad9abdc0483
alexheil <aheil4250@gmail.com>
2018-06-01 00:58:20 -0400

    purchases

--- app/controllers/purchases/purchases_controller.rb
+++ app/controllers/purchases/purchases_controller.rb
@@ -10,82 +10,76 @@ class Purchases::PurchasesController < ApplicationController
     @purchase.student_id = @student.id
     @purchase.instructor_id = @instructor.id
     
-    if @purchase.save
+    Stripe.api_key = "sk_test_ECd3gjeIEDsGkySmF8FQOC5i"
+    customer = Stripe::Customer.retrieve(current_student.customer_id)
 
-      Stripe.api_key = "sk_test_ECd3gjeIEDsGkySmF8FQOC5i"
-      customer = Stripe::Customer.retrieve(current_student.customer_id)
+    if customer.default_source.present? && @purchase.use_your_card?
 
-      if customer.default_source.present? && @purchase.use_your_card?
+      amount = ((@course.price) * 100).to_i
+      application_fee = (((@course.price * 0.129) + 0.30) * 100).round
 
-        amount = ((@course.price) * 100).to_i
-        application_fee = (((@course.price * 0.129) + 0.30) * 100).round
-
-        begin
-          charge = Stripe::Charge.create(
-            amount: amount,
-            application_fee: application_fee,
-            currency: @instructor.merchant.currency,
-            destination: @instructor.merchant.stripe_id,
-            customer: customer.id,
-            description: "#{@course.title} | Ninth Valley",
-            statement_descriptor: "NINVAL | " + @course.title[0...13].upcase.gsub(/[^a-z0-9\s]/i, '')
-          )
-        rescue Stripe::CardError => e
-          # The card has been declined
-          render 'checkout'
-          flash.now[:alert] = "Your card has been declined."
-        end
-
-        if charge.save
-          @purchase.update_attributes(
-            stripe_charge_id: charge.id
-          )
-          # send_purchase_email
-          redirect_to instructor_course_path(@instructor, @course)
-          flash[:notice] = "You have successfully purchased #{@course.title}!"
-        else
-          render 'checkout'
-          flash.now[:alert] = "You have failed."
-        end
-
-      else
-
-        token = params[:stripeToken]
-        amount = ((@course.price) * 100).to_i
-        application_fee = (((@course.price * 0.129) + 0.30) * 100).round
-
-        begin
-          charge = Stripe::Charge.create(
-            amount: amount,
-            application_fee: application_fee,
-            currency: @instructor.merchant.currency,
-            destination: @instructor.merchant.stripe_id,
-            source: token,
-            description: "#{@course.title} | Ninth Valley",
-            statement_descriptor: "NINVAL | " + @course.title[0...13].upcase.gsub(/[^a-z0-9\s]/i, '')
-          )
-        rescue Stripe::CardError => e
-          # The card has been declined
-          render 'checkout'
-          flash.now[:alert] = "You have failed."
-        end
-
-        if charge.save
-          @purchase.update_attributes(
-            stripe_charge_id: charge.id
-          )
-          # send_purchase_email
-          redirect_to instructor_course_path(@instructor, @course)
-          flash[:notice] = "You have successfully purchased #{@course.title}!"
-        else
-          render 'checkout'
-          flash.now[:alert] = "Your card has been declined."
-        end
-
+      begin
+        charge = Stripe::Charge.create(
+          amount: amount,
+          application_fee: application_fee,
+          currency: @instructor.merchant.currency,
+          destination: @instructor.merchant.stripe_id,
+          customer: customer.id,
+          description: "#{@course.title} | Ninth Valley",
+          statement_descriptor: "NINVAL | " + @course.title[0...13].upcase.gsub(/[^a-z0-9\s]/i, '')
+        )
+      rescue Stripe::CardError => e
+        # The card has been declined
+        render 'checkout'
+        flash.now[:alert] = "Your card has been declined."
       end
+
+      if charge.save
+        @purchase.update_attributes(
+          stripe_charge_id: charge.id
+        )
+        # send_purchase_email
+        redirect_to instructor_course_path(@instructor, @course)
+        flash[:notice] = "You have successfully purchased #{@course.title}!"
+      else
+        render 'checkout'
+        flash.now[:alert] = "You have failed."
+      end
+
     else
-      redirect_to instructor_course_path(@instructor, @course)
-      flash.now[:alert] = "You have failed."
+
+      token = params[:stripeToken]
+      amount = ((@course.price) * 100).to_i
+      application_fee = (((@course.price * 0.129) + 0.30) * 100).round
+
+      begin
+        charge = Stripe::Charge.create(
+          amount: amount,
+          application_fee: application_fee,
+          currency: @instructor.merchant.currency,
+          destination: @instructor.merchant.stripe_id,
+          source: token,
+          description: "#{@course.title} | Ninth Valley",
+          statement_descriptor: "NINVAL | " + @course.title[0...13].upcase.gsub(/[^a-z0-9\s]/i, '')
+        )
+      rescue Stripe::CardError => e
+        # The card has been declined
+        render 'checkout'
+        flash.now[:alert] = "You have failed."
+      end
+
+      if charge.save
+        @purchase.update_attributes(
+          stripe_charge_id: charge.id
+        )
+        # send_purchase_email
+        redirect_to instructor_course_path(@instructor, @course)
+        flash[:notice] = "You have successfully purchased #{@course.title}!"
+      else
+        render 'checkout'
+        flash.now[:alert] = "Your card has been declined."
+      end
+
     end
   end
 
