ddf177403fbfe0bb0b7f169c564677ceaf2ff2a9
alexheil <aheil4250@gmail.com>
2018-06-27 16:29:28 -0400

    test

--- app/models/video_uploader.rb
+++ app/models/video_uploader.rb
@@ -5,4 +5,12 @@ class VideoUploader < Shrine
   plugin :determine_mime_type
   plugin :cached_attachment_data
   plugin :remove_attachment
+  plugin :add_metadata
+
+  add_metadata do |io|
+    video = FFMPEG::Video.new(io.path)
+    { "duration" => video.duration}
+  end
+
+  metadata_method :duration
 end
