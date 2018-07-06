ad2deaa93f3895294d2e8cd25466e0e8ba49bfb1
alexheil <aheil4250@gmail.com>
2018-07-06 12:55:18 -0400

    bug fixes

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -23,4 +23,6 @@ end
 Shrine.plugin :activerecord
 Shrine.plugin :logging, logger: Rails.logger
 Shrine.plugin :validation_helpers
-Shrine.plugin :presign_endpoint
+Shrine.plugin :cached_attachment_data
+Shrine.plugin :restore_cached_data
+Shrine.plugin :presign_endpoint, presign_options: { method: :put }
