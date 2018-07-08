517edb0ed4b0aa6cf14b843aa5a02765bf92836f
alexheil <aheil4250@gmail.com>
2018-07-07 21:16:07 -0400

    bug fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -712,58 +712,60 @@ $(document).on('turbolinks:load', emailTabs);
 
 var directUpload = function() {
 
-function fileUpload(fileInput) {
-  var imagePreview = document.querySelector('.upload-preview')
+  function fileUpload(fileInput) {
+    var imagePreview = document.querySelector('.upload-preview');
 
-  fileInput.style.display = 'none' // uppy will add its own file input
+    fileInput.style.display = 'none' // uppy will add its own file input
 
-  var uppy = Uppy.Core({
-      id: fileInput.id,
-    })
-    .use(Uppy.FileInput, {
-      target: fileInput.parentNode,
-    })
-    .use(Uppy.Informer, {
-      target: fileInput.parentNode,
-    })
-    .use(Uppy.ProgressBar, {
-      target: imagePreview.parentNode,
-    })
+    var uppy = Uppy.Core({
+        id: fileInput.id,
+      })
+      .use(Uppy.FileInput, {
+        target: fileInput.parentNode,
+      })
+      .use(Uppy.Informer, {
+        target: fileInput.parentNode,
+      })
+      .use(Uppy.ProgressBar, {
+        target: imagePreview.parentNode,
+      });
 
-  uppy.use(Uppy.AwsS3, {
-    getUploadParameters: function (file) {
-      return fetch('/presign?filename=' + file.name, { // Shrine's presign endpoint
-        credentials: 'same-origin', // send cookies
-      }).then(function (response) { return response.json() })
-    }
-  })
-
-  uppy.on('upload-success', function (file, data, uploadURL) {
-    // show image preview
-    imagePreview.src = URL.createObjectURL(file.data)
-
-    // construct uploaded file data in the format that Shrine expects
-    var uploadedFileData = JSON.stringify({
-      id: uploadURL.match(/\/cache\/([^\?]+)/)[1], // extract key without prefix
-      storage: 'cache',
-      metadata: {
-        size:      file.size,
-        filename:  file.name,
-        mime_type: file.type,
+    uppy.use(Uppy.AwsS3, {
+      getUploadParameters: function (file) {
+        return fetch('/presign?filename=' + file.name, { // Shrine's presign endpoint
+          credentials: 'same-origin', // send cookies
+        }).then(function (response) { return response.json() })
       }
+    });
+
+    uppy.on('upload-success', function (file, data, uploadURL) {
+      // show image preview
+      imagePreview.src = URL.createObjectURL(file.data)
+
+      // construct uploaded file data in the format that Shrine expects
+      var uploadedFileData = JSON.stringify({
+        id: uploadURL.match(/\/cache\/([^\?]+)/)[1], // extract key without prefix
+        storage: 'cache',
+        metadata: {
+          size:      file.size,
+          filename:  file.name,
+          mime_type: file.type,
+        }
+      });
+
+      // set hidden field value to the uploaded file data so that it's submitted with the form as the attachment
+      var hiddenInput = fileInput.parentNode.querySelector('.upload-hidden')
+      hiddenInput.value = uploadedFileData
     })
 
-    // set hidden field value to the uploaded file data so that it's submitted with the form as the attachment
-    var hiddenInput = fileInput.parentNode.querySelector('.upload-hidden')
-    hiddenInput.value = uploadedFileData
-  })
+    return uppy
+  };
 
-  return uppy
-}
+  document.querySelectorAll('.upload-file').forEach(function (fileInput) {
+    fileUpload(fileInput)
+  });
 
-document.querySelectorAll('.upload-file').forEach(function (fileInput) {
-  fileUpload(fileInput)
-})
+};
 
 $(document).ready(directUpload);
 $(document).on('turbolinks:load', directUpload);
